c43ed860efa3e7ba39b8a2c74b189ee95697d34e
arturtsarikovich <artur.tsar.tsarikovich@gmail.com>
2024-11-27 07:31:14 +0400

    Prepare project for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -78,9 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-        ssl_require=True
+        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
     )
 }
 
